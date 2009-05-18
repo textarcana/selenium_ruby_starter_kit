ad57aa48e8f860d8b723f27664912f9559a33962
nsussman <nsussman@NYCD887.bnweb.user.bn>
2009-05-18 16:43:25 -0400

    a place to put tests you have created with Selenium IDE

---
+++ test_cases/tc_yours.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# :title:Your Tests
+# = Name
+# Your Tests
+# = Description
+# 0. Record a test using the Selenium IDE
+# 0. Once you are satisfied with your test, click on Options > Format > Ruby - Selenium RC
+# 0. Copy the test_new function and paste it into test_cases/tc_yours.rb
+# 0. Run the following command
+#     ruby ts_run_all_tests.rb http://site_you_are_testing.com
+
+require "selenium_helper"
+
+# == Run tests you have created with the Selenium IDE
+class YourTest < Test::Unit::TestCase
+
+  include SeleniumHelper
+
+  # === Your tests go here:
+
+
+
+end
+
