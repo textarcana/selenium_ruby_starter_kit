66d86ca7863ea05f67108f5720bb9ce95c79bd20
nsussman <nsussman@NYCD887.bnweb.user.bn>
2009-05-18 15:53:15 -0400

    Any tests in the test_cases directory will be run, even if they are in subdirectories.

--- ts_run_all_tests.rb
+++ ts_run_all_tests.rb
@@ -1,4 +1,4 @@
-# Run all the test cases in the test_cases directory.
+# Run all the test cases in the test_cases directory or its subdirectories.
 # Test case files are assumed to have names like tc_mumble.rb
 
-Dir.glob(File.join(File.dirname(__FILE__), 'test_cases/tc_*.rb')).each {|f| require f }
+Dir.glob(File.join(File.dirname(__FILE__),  'test_cases', '**', 'tc_*.rb')).each {|f| require f }
