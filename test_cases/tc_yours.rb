cebf67345a74cb28c4a199cef7a4d7df4b78bf89
nsussman <nsussman@NYCD887.bnweb.user.bn>
2009-05-18 16:53:52 -0400

    added an empty placeholder test because otherwise the test case fails for not specifying any tests!

--- test_cases/tc_yours.rb
+++ test_cases/tc_yours.rb
@@ -3,11 +3,17 @@
 # = Name
 # Your Tests
 # = Description
+#
+# This file is where you should paste tests you have created with the Selenium IDE.
+#
+# 0. If you haven't done so already, delete the test_cases/google_examples directory
 # 0. Record a test using the Selenium IDE
 # 0. Once you are satisfied with your test, click on Options > Format > Ruby - Selenium RC
 # 0. Copy the test_new function and paste it into test_cases/tc_yours.rb
 # 0. Run the following command
 #     ruby ts_run_all_tests.rb http://site_you_are_testing.com
+#
+# It is a good practice to rename your test_new function to something else -- test_mumble, test_foo, test_whatever -- as long as the name still begins with 'test_'.
 
 require "selenium_helper"
 
@@ -16,7 +22,12 @@ class YourTest < Test::Unit::TestCase
 
   include SeleniumHelper
 
-  # === Your tests go here:
+  # === An empty placeholder, because test cases must contain at least one test.
+  # Feel free to delete this.
+  def test_placeholder
+  end
+
+  # === Your test(s) here:
 
 
 
